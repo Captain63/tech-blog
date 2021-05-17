9904e6ca9ac537288991f6c53f508529b0af5f6d
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-17 00:01:13 -0400

    Enable routes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -31,11 +31,14 @@ app.set('view engine', 'handlebars');
 
 app.use(session(sess));
 
+// Configure server to accept json requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Allow access to the public directory for client
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
