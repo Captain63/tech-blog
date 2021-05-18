c26828e39a7af2ce7e1d82abb6d2c1f59e04a09a
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-17 23:29:35 -0400

    Create formatDate + formatDateAndTime functions

--- utils/date.js
+++ utils/date.js
@@ -1,3 +1,7 @@
-const formatDate = date => date.toLocaleTimeString();
-
-module.exports = formatDate;
+module.exports = {
+    // For blog posts
+    formatDate: (date) => `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`,
+    
+    // For comments
+    formatDateAndTime: (date) => `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()} &ndash; ${new Date(date).toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}`
+}
