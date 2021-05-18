c89ffde25df04b87edeafbed1f03803f2afdad38
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-05-17 23:29:09 -0400

    Switch helpers import to date.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/auth');
+const helpers = require('./utils/date');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
