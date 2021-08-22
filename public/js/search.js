13dec0c1bea6c385dac15cb00216a0ea4f78fbd1
Captain63 <58921221+Captain63@users.noreply@github.com>
2021-08-22 18:14:13 -0400

    Simplify functions

--- public/js/search.js
+++ public/js/search.js
@@ -3,35 +3,31 @@
 // Tag search bar
 const searchBar = document.querySelector("#search-bar");
 
-const findTerms = () => {
-    // Creates array of all searchable fields from patient records
-    const searchArray = Array.from(document.querySelectorAll(".search-container"));
+// Create array of all post containers
+const searchArray = Array.from(document.querySelectorAll(".search-container"));
 
-    for (let i = 0; i < searchArray.length; i++) {
-        // Creates empty strings property to hold retrieved values for All search
-        searchArray[i].strings = [];
+for (let i = 0; i < searchArray.length; i++) {
+    // Creates empty strings property to hold retrieved values from fields
+    searchArray[i].strings = [];
 
-        const searchTerms = searchArray[i].querySelectorAll(".search-content");
+    const searchTerms = searchArray[i].querySelectorAll(".search-content");
 
-        searchTerms.forEach(term => {
-            const string = term.innerText.toLowerCase();
+    searchTerms.forEach(term => {
+        const string = term.innerText.toLowerCase();
 
-            searchArray[i].strings.push(string);
-        })
+        searchArray[i].strings.push(string);
+    })
 
-        // Combines individual strings into combined searchString value for each record for easy referencing under displayResults
-        searchArray[i].searchString = searchArray[i].strings.join(" ");
-    }
-
-    return searchArray;
+    // Combines individual strings into combined searchString value for each record for easy referencing under displayResults
+    searchArray[i].searchString = searchArray[i].strings.join(" ");
 }
 
 // Function for filtering out non-matching results on page
 const displayResults = (array, query) => {
-    // Finds list items that match
+    // Finds post content that matches
     const matchArray = array.filter(item => item.searchString.includes(query));
 
-    // Finds list items that DON'T match
+    // Finds post content that doesn't match
     const noMatchArray = array.filter(item => !item.searchString.includes(query));
 
     // Displays those that match
@@ -48,11 +44,7 @@ const displayResults = (array, query) => {
 }
 
 const searchQuery = ({ target }) => {
-
-    // Captures string entered into input
-    const query = target.value.toLowerCase();
-
-    displayResults(findTerms(), query);
+    displayResults(searchArray, target.value.toLowerCase());
 }
 
 // Assigns event listener each time key is entered into input
